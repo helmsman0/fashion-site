27a8d7fff6a59cad285db8c261c77f7d0b83f9b6
helmsman0 <121854293+helmsman0@users.noreply.github.com>
2023-10-31 03:39:42 +0530

    Delete front-end directory

--- front-end/Pages/about.js
+++
@@ -1,9 +0,0 @@
-const sr = ScrollReveal({
-    distance: '50px',
-    duration: 2550,
-    reset: true
-});
-
-sr.reveal('header',{delay:300});
-sr.reveal('.container',{delay:400, origin: 'top'});
-sr.reveal('.container p h2',{delay:510, origin: 'left'});
--- front-end/Pages/contact.js
+++
@@ -1,7 +0,0 @@
-const sr = ScrollReveal({
-    distance: '50px',
-    duration: 2550,
-    reset: true
-});
-
-sr.reveal('.container',{delay:300});
